e9197169d08f61ac28710b123883120426df8a95
Steven Huang <steven.charles.huang@gmail.com>
2018-06-25 23:15:58 -0400

    react server running concurrently with express server, socket server is standing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import socket from './socket/api';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  componentDidMount(){
+
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
